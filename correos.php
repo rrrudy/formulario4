1d0345cf19f65bc3181ff9c9e5be1102527574bd
rrrudy <65423817+rrrudy@users.noreply.github.com>
2021-12-16 09:13:13 -0500

    Add files via upload

---
+++ correos.php
@@ -0,0 +1,13 @@
+<?php
+$destinatario ='rudykactuss1@gmail.com';
+$nombre = $_POST['nombre'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+$email = $_POST['email'];
+$header ="enviado desde la pagina de rudy";
+$mensajecompleto = $mensaje . "\nAtentamente: " . $nombre;
+mail($destinatario, $asunto, $mensaje, $header);
+echo "<script>alert('correo enviado exitosamente)</script>";
+echo "<script> setTimeout(\"location.href='index.html'\" ,1000)</script>";
+
+?>
